d2b1791d1e7a692750489c6ecaacd95a23049206
BhagyeshPro <bhagyeshpaunikar2001@gmail.com>
2023-01-14 18:01:57 +0530

    DEv

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>CHILLMATE</Text>
+      <Text style={styles.text}>CHILLMATE Dev</Text>
       <StatusBar style="auto" />
     </View>
   );
